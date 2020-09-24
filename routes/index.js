5967d8d9ce21f9eee70c236c10d180eb58468dc7
Rui Yang <ryang@bglcorp.com.au>
2020-09-24 16:38:18 +1000

    add iat exp claim in token

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,22 @@ var router = express.Router();
 var getKeyStore = require('../jwt/keyStore.js').getKeyStore;
 var jose = require('node-jose');
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.json({hello: "world!"});
 });
 
-
 router.get('/.well-known/jwks.json', async function(req, res) {
-  console.log(getKeyStore);
   var keys = await getKeyStore();
   res.json(keys.toJSON());
-//  res.json(keystore.toJSON());
 });
 
 router.post('/token', async function(req, res) {
   var keys = await getKeyStore();
   var key = keys.all()[0];
+  var body = req.body;
+  body["iat"] = Math.floor(Date.now() / 1000);
+  body["exp"] = Math.floor(Date.now() / 1000) + 3600;
   var token = await new Promise((resolve) => {
     jose.JWS.createSign({ alg: 'RS256', format: 'compact' }, key).
       update(JSON.stringify(req.body)).
