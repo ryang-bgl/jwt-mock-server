60ab53955dd0ece34c4f229b6f742248d2f5e6a1
Rui Yang <ryang@bglcorp.com.au>
2020-09-24 15:44:51 +1000

    create jwt token

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var getKeyStore = require('../jwt/keyStore.js').getKeyStore;
+var jose = require('node-jose');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +14,21 @@ router.get('/.well-known/jwks.json', async function(req, res) {
   var keys = await getKeyStore();
   res.json(keys.toJSON());
 //  res.json(keystore.toJSON());
+});
+
+router.post('/token', async function(req, res) {
+  var keys = await getKeyStore();
+  var key = keys.all()[0];
+  var token = await new Promise((resolve) => {
+    jose.JWS.createSign({ alg: 'RS256', format: 'compact' }, key).
+      update(JSON.stringify(req.body)).
+      final().
+      then(function(result) {
+        resolve(result);
+      });
+
+  });
+  res.json({token: token});
 });
 
 
