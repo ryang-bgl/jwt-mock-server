0df271a5c913c5edb452e0490e24499c024ee003
Rui Yang <ryang@bglcorp.com.au>
2020-09-29 15:38:59 +1000

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var logger = require('morgan');
 
@@ -11,6 +12,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/jwt', indexRouter);
 
